4b1341dc70c6507d5e7c887c6d81a41cca450207
edsonjfs <edsonjosefs@gmail.com>
2022-02-07 16:36:22 -0300

    refactor: changed tests to expect HTTP reponse status code

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -14,28 +14,28 @@ RSpec.describe 'Application Controller App' do
   describe 'index page "/"' do
     it 'test index page HTTP status' do
       get '/'
-      expect(last_response).to be_ok
+      expect(last_response.status).to eq(200)
     end
   end
 
   describe '/graduation_info page' do
     it 'test graduation_info page HTTP status' do
       get '/graduation_info'
-      expect(last_response).to be_ok
+      expect(last_response.status).to eq(200)
     end
   end
 
   describe '/pull_requests page' do
     it 'test pull_requests page HTTP status' do
       get '/pull_requests'
-      expect(last_response).to be_ok
+      expect(last_response.status).to eq(200)
     end
   end
 
   describe 'inexistent page' do
     it 'test accessing an inexistent url' do
       get '/fake_url'
-      expect(last_response).to_not be_ok
+      expect(last_response.status).to eq(404)
     end
   end
 end
